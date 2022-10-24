6c9d4a8ccce72e4a7cb934c368851ce3280282cc
flux <25628292+fluxionary@users.noreply.github.com>
2022-10-24 10:34:21 -0700

    check futil version

--- init.lua
+++ init.lua
@@ -1,6 +1,11 @@
 local modname = minetest.get_current_modname()
 local modpath = minetest.get_modpath(modname)
 
+assert(
+	type(futil.version) == "number" and futil.version >= os.time({year = 2022, month = 10, day = 24}),
+	"please update futil"
+)
+
 action_queues = {
 	version = os.time({year = 2022, month = 8, day = 16}),
 
